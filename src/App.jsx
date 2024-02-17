432c54c378338885be6addab9b921a77d78836c7
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-02-17 11:27:38 +0200

    task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getUserInfo } from './service';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      getUserInfo(pos.coords);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
